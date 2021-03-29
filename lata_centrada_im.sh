fe6fa6ba099c524c6cb4d664ba45e7e529ac2682
Alejandro Gomez-Garay <gomezgaray.alejandro@gmail.com>
2021-03-29 11:57:59 -0600

    Add files via upload

---
+++ lata_centrada_im.sh
@@ -0,0 +1,16 @@
+convert -size 600x600 xc:white \
+-fill white -stroke black -draw "circle 300,300 570,300" \
+-fill snow2 -stroke black -draw "circle 300,300 550,300" \
+-fill snow2 -stroke black -draw "circle 300,300 500,300" \
+-fill snow2 -stroke black -draw "circle 300,300 490,300" \
+-fill snow2 -stroke black -draw "circle 300,300 480,300" \
+-fill snow2 -stroke black -draw "circle 300,300 470,300" \
+-fill snow2 -stroke black -draw "circle 300,300 460,300" \
+-fill snow2 -stroke black -draw "circle 300,300 450,300" \
+-fill snow2 -stroke black -draw "circle 300,300 390,300" \
+-fill snow3 -stroke black -draw "circle 300,300 355,300" \
+-fill snow2 -stroke black -draw "circle 300,300 350,300" \
+-fill snow2 -stroke black -draw "line 390,300 550,300" \
+-fill snow2 -stroke black -draw "line 390,295 550,275" \
+-fill snow2 -stroke black -draw "line 390,305 550,325" \
+canvas_centrado.png
---
+++ lata_desviada_im.sh
@@ -0,0 +1,16 @@
+convert -size 600x600 xc:white \
+-fill white -stroke black -draw "circle 300,300 570,300" \
+-fill snow2 -stroke black -draw "circle 300,300 550,300" \
+-fill snow2 -stroke black -draw "circle 300,280 500,280" \
+-fill snow2 -stroke black -draw "circle 300,280 490,280" \
+-fill snow2 -stroke black -draw "circle 300,280 480,280" \
+-fill snow2 -stroke black -draw "circle 300,280 470,280" \
+-fill snow2 -stroke black -draw "circle 300,280 460,280" \
+-fill snow2 -stroke black -draw "circle 300,280 450,280" \
+-fill snow2 -stroke black -draw "circle 300,280 390,280" \
+-fill snow3 -stroke black -draw "circle 300,280 355,280" \
+-fill snow2 -stroke black -draw "circle 300,280 350,280" \
+-fill snow2 -stroke black -draw "line 390,280 550,280" \
+-fill snow2 -stroke black -draw "line 390,275 550,255" \
+-fill snow2 -stroke black -draw "line 390,285 550,305" \
+canvas_desviado.png
